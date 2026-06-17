3d9ba07a5bc1f6bdc5612da98b809cd87f603eec
Andrishel <laam.07.2015@gmail.com>
2026-06-17 05:04:42 -0500

    movil: integrar permisos GPS y logo oficial de MOTOGUARD

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vlink.app',
+  appName: 'V-LINK',
+  webDir: 'build'
+};
+
+export default config;
